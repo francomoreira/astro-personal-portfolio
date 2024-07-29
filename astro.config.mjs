6c25a8955d6e66ac5173faf328172f1faa7f0ea6
Franco Moreira <hidoggy.music@gmail.com>
2024-07-28 22:26:16 -0300

    [ADD] 📮 blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
 
+import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://my.franmore.ar/',
-  integrations: [sitemap()]
+  integrations: [mdx(), sitemap()],
 });
